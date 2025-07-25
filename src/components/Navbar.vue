729d43cb30184452f161697ff78c59560035fad9
luizfeliperomero <luizfeliperomero10@gmail.com>
2025-07-25 14:18:16 -0300

    Highlight active route

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,13 +3,19 @@
     import toggleLight from '@/assets/svg/toggle-light.svg';
     import toggleDark from '@/assets/svg/toggle-dark.svg';
     import { ref, computed, onMounted } from 'vue'
-    import { RouterLink } from 'vue-router';
+    import { RouterLink, useRoute } from 'vue-router';
 
     const isDark = ref(false);
 
     onMounted(() => {
 	isDark.value = document.body.classList.contains('dark');
     })
+
+    const isActiveLink = (routePath) => {
+	const route = useRoute();
+	return route.path === routePath;
+    }
+
     const toggleDarkMode = () => {
 	document.body.classList.toggle('dark');
 	isDark.value = document.body.classList.contains('dark');
@@ -21,8 +27,8 @@
     <nav class="flex p-2 justify-between items-center bg-bt-primary w-full">
 	<img class="h-20" :src="logo" alt="Bike Tracker" />
 	<div class="flex justify-center items-center gap-10">
-	    <RouterLink to="/" class="text-white font-medium hover:text-bt-secondary">Home</RouterLink>
-	    <RouterLink to="/adminPanel" class="text-white font-medium hover:text-bt-secondary">Admin Panel</RouterLink>
+	    <RouterLink to="/" :class="['-white font-medium hover:text-bt-secondary', isActiveLink('/') ? 'text-bt-secondary': 'text-white']">Home</RouterLink>
+	    <RouterLink to="/adminPanel" :class="['font-medium hover:text-bt-secondary', isActiveLink('/adminPanel') ? 'text-bt-secondary': 'text-white']">Admin Panel</RouterLink>
 	</div>
 	<img class="h-6 cursor-pointer" :src="toggleIcon" alt="Bike Tracker" @click="toggleDarkMode"/>
     </nav>
